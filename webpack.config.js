42fdb88386e8470ee174e43a106ae67c1095979a
greinisch <greinisch@youwol.com>
2022-07-08 11:17:52 +0200

    🔧 webpack.config.js => add bundle-analyzer + auto-format code

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,60 @@
-const path = require('path');
-const webpack = require('webpack');
-const pkg = require('./package.json');
-const ROOT = path.resolve(__dirname, 'src');
-const DESTINATION = path.resolve(__dirname, 'dist');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
+const webpack = require('webpack')
+const pkg = require('./package.json')
+const ROOT = path.resolve(__dirname, 'src')
+const DESTINATION = path.resolve(__dirname, 'dist')
+const BundleAnalyzerPlugin =
+    require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     context: ROOT,
     entry: {
-        'main': './index.ts'
+        main: './index.ts',
     },
     plugins: [
-        //new BundleAnalyzerPlugin()
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            reportFilename: './bundle-analysis.html',
+            openAnalyzer: false,
+        }),
     ],
     output: {
         path: DESTINATION,
         libraryTarget: 'umd',
         umdNamedDefine: true,
         library: pkg.name,
-        filename: pkg.name + ".js",
-        globalObject: `(typeof self !== 'undefined' ? self : this)`
+        filename: pkg.name + '.js',
+        globalObject: `(typeof self !== 'undefined' ? self : this)`,
     },
     resolve: {
         extensions: ['.ts', 'tsx', '.js'],
-        modules: [
-            ROOT,
-            'node_modules'
-        ]
+        modules: [ROOT, 'node_modules'],
     },
-    externals: [{
-        '@youwol/dataframe': "@youwol/dataframe",
-        "@youwol/geometry": "@youwol/geometry",
-        'three': {
-            commonjs:'three',
-            commonjs2:'three',
-            root: 'THREE'
+    externals: [
+        {
+            '@youwol/dataframe': '@youwol/dataframe',
+            '@youwol/geometry': '@youwol/geometry',
+            three: {
+                commonjs: 'three',
+                commonjs2: 'three',
+                root: 'THREE',
+            },
+            'three-trackballcontrols': {
+                commonjs: 'three-trackballcontrols',
+                commonjs2: 'three-trackballcontrols',
+                root: ['TrackballControls'],
+            },
+            '@youwol/three-extra': '@youwol/three-extra',
         },
-        "three-trackballcontrols": {
-            commonjs:"three-trackballcontrols",
-            commonjs2:"three-trackballcontrols",
-            root:["TrackballControls"]
-        },
-        '@youwol/three-extra': "@youwol/three-extra"
-    }],
+    ],
     module: {
         rules: [
             {
                 test: /\.ts$/,
-                use: [
-                    { loader: 'ts-loader' },
-                  ],
-                  exclude: /node_modules/,
-            }
+                use: [{ loader: 'ts-loader' }],
+                exclude: /node_modules/,
+            },
         ],
     },
-    devtool: 'source-map'
-};
+    devtool: 'source-map',
+}
